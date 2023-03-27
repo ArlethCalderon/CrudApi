eb5482076d68811762efb3827aca598c8ffc1f31
Arleth Calderon <arletvalle78@gmail.com>
2023-03-27 12:33:51 -0700

    login

--- src/componente/Formulario.js
+++ src/componente/Formulario.js
@@ -1,8 +1,28 @@
+import { useState, useEffect } from "react";
+import { Navigate } from "react-router-dom";
+
 const Formulario = () => {
+    
+    const [loggedIn, setLoggedIn] = useState(false);
+    const [username, setUsername] = useState('');
+  
+    useEffect(() => {
+      const storedUsername = localStorage.getItem('username');
+      if (storedUsername) {
+        setLoggedIn(true);
+        setUsername(storedUsername);
+      }
+    }, []);
+  
+    if (!loggedIn) {
+      return <Navigate to="/" />;
+    }
+  
     return (
-        <div>
-            <h1>Formulario</h1>
-        </div>
-    )
+      <div>
+        <h1>Welcome, {username}!</h1>
+        {/* TODO: add the rest of your app */}
+      </div>
+    );
 }
 export default Formulario
--- src/componente/Login.js
+++ src/componente/Login.js
@@ -1,18 +1,42 @@
+import React from "react"
+import { useState } from "react";
+
+
 const Login = () => {
+    
+
+    const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleUsernameChange = (event) => {
+    setUsername(event.target.value);
+  };
+
+  const handlePasswordChange = (event) => {
+    setPassword(event.target.value);
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    // TODO: handle login logic
+    if (username === 'myusername' && password === 'mypassword') {
+        localStorage.setItem('username', username);
+      }
+      
+  };
+
+
     return (
         <div>
-            <form>
+            <form onSubmit={handleSubmit}>
             <div class="mb-3">
-                 <label for="exampleInputEmail1" class="form-label">Nombre Usuario</label>
-                 <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" />
+                 <label htmlFor="username" class="form-label">Nombre Usuario</label>
+                 <input type="text" id="username" value={username} onChange={handleUsernameChange} class="form-control" />
             </div>
 
             <div class="mb-3">
-                <label for="exampleInputPassword1" class="form-label">Contraseña</label>
-                <input type="password" class="form-control" id="exampleInputPassword1" />
-            </div>
-                <div class="mb-3 form-check">
-                 <input type="checkbox" class="form-check-input" id="exampleCheck1" />
+                <label htmlFor="password" class="form-label">Contraseña</label>
+                <input type="password" id="password" value={password} onChange={ handlePasswordChange } class="form-control"  />
             </div>
                 <button type="submit" class="btn btn-primary">Iniciar Sesion</button>
                 </form>
